57c29b5a161a9a445a6b4affa35426eef75668be
zhaoran <zhaoran.wu1@gmail.com>
2020-07-07 01:50:03 +0200

    add new movement predection, add hybrid depth map for tracking, debug

--- src/base/config.h
+++ src/base/config.h
@@ -42,11 +42,11 @@ class Config {
     float OPTIMIZATION_LAMDA_FAILED_PENALIZE;
     float OPTIMIZATION_LAMDA_SUCCESS_PENALIZE;
 
-    const int max_iteration_each_lvl[5] = {50, 30, 15, 10, 10};
+    const int max_iteration_each_lvl[5] = {50, 30, 25, 20, 10};
     const float lamda_init_each_lvl[5] = {
-        1e-8, 1e-5, 1e-2, 1, 1};  // very sensitive : e.g motion blur
-    const float lamda_min_eahc_lvl[5] = {1e-14, 1e-10, 1e-9, 1e-8, 1e-7};
-    const int huber_residual_each_lvl[5] = {30, 45, 55, 80, 100};
+        1e-8, 1e-5, 1e-1, 1, 1};  // very sensitive : e.g motion blur
+    const float lamda_min_eahc_lvl[5] = {1e-14, 1e-10, 1e-9, 1e-7, 1e-7};
+    const int huber_residual_each_lvl[5] = {45, 55, 75, 120, 120};
 
     static const int WINDOW_SIZE = 7;
 };
